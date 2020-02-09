3656b08aee33b61bbade0a88d01a4b8d7fc12558
webturing <zj@webturing.com>
2020-02-09 14:48:47 +0800

    原位合并操作，需要O(N）的辅助空间

---
+++ 啊哈算法/lec01-soring/p06-mergesort2.js
@@ -0,0 +1,37 @@
+/**
+ * Created by webturing on 2020/2/9.
+ */
+
+
+
+
+
+function main() {
+    let x = [1, 3, 5, 7, 9, 2, 4, 5, 8, 10, 14];
+    let mid = 4;
+    let a = [1, 3, 5, 7, 9,];
+    let b = [2, 4, 5, 8, 10, 14];
+    let c = new Array(a.length + b.length);
+    let i = 0, j = 0, k = 0;
+    while (i < a.length && j < b.length) {
+        if (a[i] <= b[j]) {
+            c[k++] = a[i++];
+        } else {
+            c[k++] = b[j++];
+        }
+    }
+
+    while (i < a.length) {
+        c[k++] = a[i++];
+
+    }
+    while (j < b.length) {
+        c[k++] = b[j++];
+
+    }
+
+    c.forEach(x => console.log(x));
+
+}
+
+main();
