3672546439bd4377a84af3520573aaf477d1c354
mac <hsq@webturing.com>
2020-02-09 13:00:30 +0800

    冒泡排序

---
+++ 啊哈算法/lec01-soring/p03-bubble2.js
@@ -0,0 +1,25 @@
+/**
+ * Created by webturing on 2020/2/9.
+ */
+function sort(arr) {
+    for (let i = 0, n = arr.length; i < n; i++) {
+        for (let j = n - 2; j > i; j--) {
+            if (arr[j] > arr[j + 1]) {
+                let t = arr[j + 1];
+                arr[j + 1] = arr[j];
+                arr[j] = t;
+            }
+
+        }
+    }
+}
+
+function main() {
+    let arr = [1, 3, 5, 7, 9, 2, 4, 6, 8, 0];
+    sort(arr);
+    arr.forEach(x => console.log(x));
+}
+
+
+main();
+
