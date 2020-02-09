81d2d08c88ea3a523d71369b50db9eefdec90af3
webturing <zj@webturing.com>
2020-02-09 14:19:28 +0800

    归并排序核心：合并两个有序列表

---
+++ 啊哈算法/lec01-soring/p06-mergesort.js
@@ -0,0 +1,19 @@
+/**
+ * Created by webturing on 2020/2/9.
+ */
+
+
+
+
+
+function main() {
+    let a = [1, 3, 5, 7, 9,];
+    let b = [2, 4, 5, 8, 10, 14];
+    let c = [];
+
+
+    c.forEach(x => console.log(x));
+
+}
+
+main();
