d540b9917b1f2a1ee56d5a0abd1b935b497a7a61
webturing <zj@webturing.com>
2020-02-10 18:28:31 +0800

    二分搜索实现（递归）

--- 啊哈算法/lec02-searching/p02-binary-searching.js
+++ 啊哈算法/lec02-searching/p02-binary-searching.js
@@ -1,21 +1,21 @@
 /**
  * Created by webturing on 2020/2/10.
  */
-function linear_search(arr, key) {//O(n)
-    let pos = -1;
-    for (let i = 0; i < arr.length; i++) {
-        if (key === arr[i]) {
-            pos = i;
-            break;
-        }
-    }
-    return pos;
+function binary_search(arr, key, left, right) {//O(log n)
+    if (left > right)return -1;
+    if (left === right)return arr[left] === key ? left : -1;
+    let mid = Math.floor((left + right) / 2);
+    if (arr[mid] === key)return mid;
+    if (arr[mid] > key)return binary_search(arr, key, left, mid - 1);
+
+    return binary_search(arr, key, mid + 1, right);
 
 }
 function main() {
     let arr = [1, 3, 5, 7, 2, 4, 6, 8, 0, 7];
+    arr.sort((a, b) => a - b);
     let key = 7;
-    console.log(arr.indexOf(key));
-    console.log(linear_search(arr, key));
+    console.log(binary_search(arr, key, 0, arr.length - 1));
+
 }
 main();
