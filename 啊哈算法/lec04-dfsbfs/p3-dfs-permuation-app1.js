973b0ffeb04b8c60ec8d3705cb8aba8f41e64a3c
webturing <zj@webturing.com>
2020-02-14 10:05:30 +0800

    N皇后无剪枝优化版本

--- 啊哈算法/lec04-dfsbfs/p3-dfs-permuation-app1.js
+++ 啊哈算法/lec04-dfsbfs/p3-dfs-permuation-app1.js
@@ -1,16 +1,16 @@
 /**
  * Created by webturing on 2020/2/13.
  */
-let c = [0, 1, 2, 3]
-let a = [0, 0, 0, 0];
-let b = [0, 0, 0, 0];
+let c = [0, 1, 2, 3, 4, 5, 6, 7,]
+let a = [0, 0, 0, 0, 0, 0, 0, 0,];
+let b = [0, 0, 0, 0, 0, 0, 0, 0];
 
 
 function dfs(k) {
-    if (k == 4) {
+    if (k == 8) {
         var flag = 1;
-        for (let i = 0; i < 4; i++)
-            for (let j = i + 1; j < 4; j++) {
+        for (let i = 0; i < 8; i++)
+            for (let j = i + 1; j < 8; j++) {
                 var slop = (a[i] - a[j]) / (i - j);
                 if (slop == 1 || slop == -1) {
                     flag = 0;
@@ -18,16 +18,16 @@ function dfs(k) {
             }
         if (flag) {
             var ans = " ";
-            for (let i = 0; i < 4; i++) {
+            for (let i = 0; i < 8; i++) {
 
                 ans += "(" + i + "," + a[i] + ")";
 
 
             }
             console.log(ans);
-            for (let i = 0; i < 4; i++) {
+            for (let i = 0; i < 8; i++) {
                 ans = '';
-                for (let j = 0; j < 4; j++) {
+                for (let j = 0; j < 8; j++) {
                     if (j === a[i])
                         ans += '[Q]';
                     else
@@ -41,7 +41,7 @@ function dfs(k) {
 
         return;
     }
-    for (let i = 0; i < 4; i++) {
+    for (let i = 0; i < 8; i++) {
         if (b[i]) continue;//
         a[k] = c[i];
         b[i] = 1;
